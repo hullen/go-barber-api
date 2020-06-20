fa29ffb0d2d2423fe106afc1dc2062179c76f409
fernandogatto <fernandogatto17@gmail.com>
2020-06-19 23:22:59 -0300

    List all providers!

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,46 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/infra/typeorm/repositories/fakes/FakeUsersRepository';
+
+import ListProvidersService from '../services/ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list the providers', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'Spaceman from Mars',
+      email: 'mars@email.com',
+      password: '123456',
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'Spaceman from Saturn',
+      email: 'saturn@email.com',
+      password: '123456',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'Spaceman from Earth',
+      email: 'earth@email.com',
+      password: '123456',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([
+      user1,
+      user2,
+    ]);
+  });
+});
--- src/modules/users/infra/typeorm/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/fakes/FakeUsersRepository.ts
@@ -2,11 +2,24 @@ import { uuid } from 'uuidv4';
 
 import IUsersRepository from '@modules/users/interfaces/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/interfaces/dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/interfaces/dtos/IFindAllProvidersDTO';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
 class UsersRepository implements IUsersRepository {
   private users: User[] = [];
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let users = this.users;
+
+    if(except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+
+    return users;
+  }
 
   public async findByEmail(email: string): Promise<User | undefined> {
     const findUser = this.users.find(user => user.email === email);

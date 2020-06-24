24429c43e8f65780134e96fb2ee3b327c69469be
fernandogatto <fernandogatto17@gmail.com>
2020-06-23 21:20:12 -0300

    Creating routes to provider month and day availability

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -46,8 +46,8 @@ class AppointmentsRepository implements IAppointmentsRepository {
     const appointments = await this.ormRepository.find({
       where: {
         provider_id,
-        date: Raw(dateFieldName =>
-          `to_char(${dateFieldName}, 'MM-YYYY' = '${parsedMonth}-${year}')`
+        data: Raw(dateFieldName =>
+          `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`
         ),
      },
     });
@@ -67,8 +67,8 @@ class AppointmentsRepository implements IAppointmentsRepository {
     const appointments = await this.ormRepository.find({
       where: {
         provider_id,
-        date: Raw(dateFieldName =>
-          `to_char(${dateFieldName}, 'DD-MM-YYYY' = '${parsedDay}-${parsedMonth}-${year}')`
+        data: Raw(dateFieldName =>
+          `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`
         ),
      },
     });
